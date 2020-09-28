3929e48e33df54987bccd032f738e387f3dd131d
Yobro49 <71135042+Yobro49@users.noreply.github.com>
2020-09-28 16:41:42 +0530

    Create Pranshu's_light-controlling-bot.py

---
+++ Pranshu's_light-controlling-bot.py
@@ -0,0 +1,27 @@
+import os
+
+x = "Mjolniemaxmuff" #ADAFRUIT_IO_USERNAME
+y = "aio_CRNm16OPH1PdNIuR3J4J75YmVKWP" #ADAFRUIT_IO_KEY
+
+from Adafruit_IO import Client, Feed
+aio = Client(x,y)
+
+from telegram.ext import Updater, CommandHandler
+import requests # Getting data from cloud
+from Adafruit_IO import Data
+
+
+def on(bot,update):
+    value = Data(value=1)
+    value_send = aio.create_data('light',value)
+
+def off(bot,update):
+    value = Data(value=0)
+    value_send = aio.create_data('light',value)
+
+u = Updater(os.getenv(u))
+dp = u.dispatcher
+dp.add_handler(CommandHandler('on',on))
+dp.add_handler(CommandHandler('off',off))
+u.start_polling()
+u.idle()
